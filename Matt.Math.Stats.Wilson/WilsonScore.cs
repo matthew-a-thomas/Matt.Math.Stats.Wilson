6b8fd8eb5035d3cead879ce95e22aabf94a2be72
Matthew Thomas <me@matthewathomas.com>
2017-12-18 11:14:09 -0500

    Implementation and tests

---
+++ Matt.Math.Stats.Wilson/WilsonScore.cs
@@ -0,0 +1,56 @@
+﻿namespace Matt.Math.Stats.Wilson
+{
+    using System;
+    using MathNet.Numerics.Distributions;
+
+    public static class WilsonScore
+    {
+        /// <summary>
+        /// Computes the lower bound of the Wilson score interval, which estimates the "actual" score of Bernoulli
+        /// tests, were they to continue forever.
+        /// </summary>
+        /// <param name="numberOfPositiveScores">The number of successful Bernoulli tests.</param>
+        /// <param name="totalScores">The total number of Bernoulli tests.</param>
+        /// <param name="confidence">Desired confidence (0-1); higher confidence results in lower score.</param>
+        /// <remarks>
+        /// https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
+        /// https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval
+        /// </remarks>
+        public static double Calculate(
+            uint numberOfPositiveScores,
+            uint totalScores,
+            double confidence)
+        {
+            if (totalScores == 0)
+                return 0;
+            
+            var z = Normal.InvCDF(
+                mean: 0,
+                stddev: 1,
+                p: 1 - (1 - confidence) / 2);
+            var zSquared = z * z;
+            
+            var pHat = 1.0 * numberOfPositiveScores / totalScores;
+            
+            var range =
+                z
+                * Math.Sqrt(
+                    pHat
+                    * (1 - pHat)
+                    / totalScores
+                    
+                    + zSquared
+                    / (4 * totalScores * totalScores)
+                )
+                / (1 + zSquared / totalScores);
+            
+            var center =
+                (pHat + (zSquared / (2 * totalScores)))
+                / (1 + zSquared / totalScores);
+            
+            var result = center - range;
+            
+            return result;
+        }
+    }
+}
---
+++ Tests/WilsonScoreClass.cs
@@ -0,0 +1,39 @@
+﻿namespace Tests
+{
+    using System;
+    using MathNet.Numerics.Distributions;
+    using Matt.Math.Stats.Wilson;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using WilsonScore;
+
+    [TestClass]
+    public class WilsonScoreClass
+    {
+        [TestClass]
+        public class CalculateMethod
+        {
+            [TestMethod]
+            public void MatchesAnotherImplementation()
+            {
+                for (var confidence = 0.01; confidence <= 0.99; confidence += 0.01)
+                {
+                    var z = Normal.InvCDF(
+                        mean: 0,
+                        stddev: 1,
+                        p: 1 - (1 - confidence) / 2);
+                
+                    for (uint total = 1; total < 100; ++total)
+                    for (uint up = 0; up <= total; ++up)
+                    {
+                        var myScore = WilsonScore.Calculate(up, total, confidence);
+                        var theirScore = Wilson.Score(up, total, z);
+                        var difference = Math.Abs(theirScore - myScore);
+                        Assert.IsTrue(
+                            difference <= 1E-15,
+                            $"Difference was {difference}: my score: {myScore}; their score: {theirScore}; up/total: {up}/{total}; confidence: {confidence}");
+                    }
+                }
+            }
+        }
+    }
+}
